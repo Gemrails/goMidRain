52cba524080253e7499fe9c328ab813cbdd9eee3
崔斌 <cuiminmm@gmail.com>
2017-07-02 02:02:36 +0800

    add main

---
+++ mid_rain.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "midrain.app/midoptions"
+	"github.com/Gemrails/midRainApp/midoptions"
+)
+
+func init() {
+}
+
+func main() {
+	midoptions.Options()
+}
+
+
+/*
+need
+KUBERNETES_SERVICE_PORT=8181
+KUBERNETES_SERVICE_HOST=http://test.goodrain.com:8181
+*/
