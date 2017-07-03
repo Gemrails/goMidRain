96586e96149aaac352d80766557f63b17c7f6bee
崔斌 <cuiminmm@gmail.com>
2017-07-03 11:56:00 +0800

    update test

--- test_main/test_main.go
+++ test_main/test_main.go
@@ -1,16 +1,15 @@
-package test_main
+package main
 
 import (
 	"fmt"
-	"strconv"
-        "midrain.app/midconst"
         "net/http"
         "io/ioutil"
         "strings"
+	"strconv"
 )
 
 func httpGet() {
-        resp, err := http.Get(midconst.Infourl)
+        resp, err := http.Get("http://172.30.0.8:65534/server_info")
         if err != nil {
                 fmt.Println("123123")
         }else{
@@ -19,12 +18,37 @@ func httpGet() {
                 if err != nil {
                         fmt.Println("sfsdfadf")
                 }else{
-                        restart_epoch, _ := strconv.Atoi(strings.Split(string(body), " ")[5])
+                        //restart_epoch, _ := strconv.Atoi(strings.Split(string(body), "")[1])
+                        restart_epoch := strings.Split(string(body), " ")[5]
+			for key, value := range restart_epoch {
+				fmt.Printf("key %v, value %s \n", key, value)
+			}
+			ne, _:= strconv.Atoi(strings.Replace(restart_epoch[5], "\n", "", -1))
+			// 此处取出的成员后面跟随了一个空格
+			fmt.Println("restart.....")
                         fmt.Println(restart_epoch)
+			fmt.Println(string(body))
+			fmt.Println(restart_epoch[5])
+			fmt.Println(ne)
                 }
         }
 }
 
+func xhttpGet() {
+	resp, err := http.Get("http://172.30.0.8:65534/server_info")
+	if err != nil {
+	}else{
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+		}else{
+			restart_epoch, _ := strconv.Atoi((strings.Replace(strings.Split(string(body), " ")[5], "\n", "", -1)))
+			fmt.Println(restart_epoch)
+		}
+	}
+}
+
 func main(){
-        httpGet
+        httpGet()
+	xhttpGet()
 }
